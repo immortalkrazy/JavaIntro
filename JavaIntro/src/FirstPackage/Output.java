ab9061a4308affe0f19a7f5354304f3ff9b0a169
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-05-27 13:44:19 -0400

    first commit to local repo

---
+++ JavaIntro/src/FirstPackage/Output.java
@@ -0,0 +1,11 @@
+package FirstPackage;
+
+public class Output {
+
+      public static void main(String[] args) {
+            // TODO Auto-generated method stub
+            System.out.println("Hello Java");
+
+      }
+
+}
