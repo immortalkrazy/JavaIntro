adfb8ed1da644c6eb2b26cc117823a2f7714b248
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2019-05-27 13:45:49 -0400

    first commit to github

--- JavaIntro/src/FirstPackage/Output.java
+++ JavaIntro/src/FirstPackage/Output.java
@@ -5,6 +5,7 @@ public class Output {
       public static void main(String[] args) {
             // TODO Auto-generated method stub
             System.out.println("Hello Java");
+            System.out.println("commit to git hub");
 
       }
 
