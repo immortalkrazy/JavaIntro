95fda4ac7d514933f2a57f3f9260e510099ad757
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2020-01-05 01:45:14 -0500

    added apartment and test apartment

---
+++ JavaIntro/src/help_programming/PasswordRandomizerTest.java
@@ -0,0 +1,15 @@
+package help_programming;
+
+public class PasswordRandomizerTest {
+
+      public static void main(String[] args) {
+
+            PasswordRandomizer randomPassword = new PasswordRandomizer(13);
+
+            System.out.println("Password: " + randomPassword.createPassword());
+            System.out.println("Password: " + randomPassword.createPassword());
+            System.out.println("Password: " + randomPassword.createPassword());
+            System.out.println("Password: " + randomPassword.createPassword());
+            System.out.println("Password: " + randomPassword.createPassword());
+      }
+}
